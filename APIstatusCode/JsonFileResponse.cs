20ddc81063e8fd4f09f1eeeb5ab7f5f3b43d483c
xCriminalStylex <oleksandrzikran@gmail.com>
2022-01-27 22:46:02 +0200

    Добавьте файлы проекта.

---
+++ APIstatusCode/JsonFileResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIstatusCode
+{
+    public class JsonFileResponse
+    {
+        public string Message { get; set; }
+        public string Cod { get; set; }
+    }
+}
---
+++ APIstatusCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace APIstatusCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string url = "http://api.openweathermap.org/data/2.5/find?q=London&units=metric&appid=c306ad20e3d780a22a50e7dd23900d02";
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                System.Threading.Thread.Sleep(10000);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                System.Threading.Thread.Sleep(10000);
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                JsonFileResponse jsonFileResponse = JsonConvert.DeserializeObject<JsonFileResponse>(response);
+                Console.WriteLine($"Status message:{jsonFileResponse.Message}");
+                Console.WriteLine($"Status code:{jsonFileResponse.Cod}");
+
+
+        }
+    }
+}
